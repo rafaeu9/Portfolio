a273dd6e99061bfebe9904d3f21b817308d8a93d
Mário Fernandes <ask.mariofernandes@gmail.com>
2025-03-08 13:02:37 +0000

    Add containers
    Adds containers for sub pages
    
    Signed-off-by: Mário Fernandes <ask.mariofernandes@gmail.com>

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ function getFolders() {
         type: 'GET',
         dataType: 'json', // Expecting a JSON response
         success: function(directories) {
-            console.log("Received directories:", directories);
             directories.forEach(function(dir) {
                 loadContent("/projects/" + dir); // Call the loadContent function for each directory
             });
@@ -31,6 +30,46 @@ function loadContent(path) {
 }
 
 $(document).ready(function() {
-    console.log("JavaScript Loaded with jQuery!");
     getFolders(); // Fetch the folders once the DOM is ready
+    
 });
+
+$.fn.shadow = function() {
+    return this[0].shadowRoot || this[0].attachShadow({ mode: "open" });
+}
+
+function createContainer(file){
+    console.log("creating a container");
+
+    var body = document.getElementsByTagName("body")[0],
+        veil = document.createElement("div"),
+        container = document.createElement("div");
+    
+    veil.id = "veil";
+    $(body).append(veil);
+
+    veil.style.display = 'block'; // Show the veil
+    body.classList.add('lock-scroll'); // Lock scrolling
+    
+    
+    container.id = "container";
+    $(body).append(container);
+
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            // Inject the file content into the shadow root
+            $(container).shadow().innerHTML = data;
+        })
+        .catch(err => console.error("Error loading file:", err));
+}
+
+function exitConainer() {
+    var container   = document.getElementById("container"),
+        veil        = document.getElementById("veil"),
+        body        = document.getElementsByTagName("body")[0];
+
+    $(container).remove();
+    $(veil).remove();
+    body.classList.remove('lock-scroll');
+}
